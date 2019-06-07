19f5f0912c8d6193115ca9e49d233311cb8ee141
Gautham Velchuru <gvelchuru@gmail.com>
2019-06-06 21:12:37 -0700

    switch to redshift-gtk

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,9 +8,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 if type "xrandr"; then
-    MONITOR=DP-0 polybar --reload top &
-    sleep 10
     MONITOR=HDMI-0 polybar --reload top &
+    sleep 10
+    MONITOR=DVI-D-0 polybar --reload top &
   #for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     #MONITOR=$m polybar --reload bottom &
   #done
