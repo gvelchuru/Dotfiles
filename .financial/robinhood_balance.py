ee5b35add9335f5f784ad2204478aed106c92f7a
Gautham Velchuru <velchug@f0189861e52b.ant.amazon.com>
2019-10-24 10:58:03 -0700

    robinhood balancer

--- .financial/robinhood_balance.py
+++ .financial/robinhood_balance.py
@@ -1,15 +1,16 @@
 import heapq
 
 stocks = [
-    ("nusc", 22, 29.58, 0.2),
-    ("nuem", 7, 25.5, 0.1),
-    ("nudm", 22, 26.68, 0.1),
-    ("numv", 3, 29.36, 0.1),
-    ("numg", 6, 32.91, 0.1),
-    ("nulc", 51, 27.69, 0.3),
-    ("nuag", 0, 24.75, 0.1),
+    ("nusc", 54, 29.51, 0.18),
+    ("nuem", 7, 25.77, 0.1),
+    ("nudm", 22, 26.73, 0.1),
+    ("numv", 3, 29.19, 0.09),
+    ("numg", 6, 33.13, 0.09),
+    ("nulc", 51, 27.69, 0.28),
+    ("nuag", 0, 24.81, 0.06),
+    ("nure", 26, 31.14, .1)
 ]
-buying_power = .16 
+buying_power = 2780.91 * .9
 total = sum(count * price for _, count, price, _ in stocks)
 
 print("SUM" + str(sum(x[3] for x in stocks)))
