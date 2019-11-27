ad48b19824a0f8b8fee5d670ac109fa3237c97f5
Gautham Velchuru <gvelchuru@gmail.com>
2019-11-27 10:14:39 -0800

    alacritty

--- zsh_scripts/tmux_setup.sh
+++ zsh_scripts/tmux_setup.sh
@@ -1,7 +1,5 @@
 if command -v tmux &> /dev/null && [[ -n "$PS1" ]] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ] && [[ ! "$UNAME" =~ "Darwin" ]]; then
   if [[ $APOLLO_EXISTS -eq 0 ]]; then
     exec tmux new-session -A -s main -c /workplace/$(whoami)
-  else 
-    exec tmux new-session -A -s main 
   fi
 fi
