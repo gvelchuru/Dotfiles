df1e4ff5455e33dab9c58ebaa83c4fce33d4c383
EC2 Default User <ec2-user@ip-172-31-42-164.us-west-2.compute.internal>
2019-12-14 04:11:05 +0000

    tmux ec2

--- zsh_scripts/tmux_setup.sh
+++ zsh_scripts/tmux_setup.sh
@@ -1,8 +1,7 @@
 if command -v tmux &> /dev/null && [[ -n "$PS1" ]] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ] && [[ ! "$UNAME" =~ "Darwin" ]]; then
   if [[ $APOLLO_EXISTS -eq 0 ]]; then
     exec tmux new-session -A -s main -c /workplace/$(whoami)
+  elif [[ $IS_EC2 -eq 0 ]]; then
+    exec tmux new-session -A -s main
   fi
 fi
-
-  #elif [[ $IS_EC2 -eq 0 ]]; then
-    #exec tmux new-session -A -s main
