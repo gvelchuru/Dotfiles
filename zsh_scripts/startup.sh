bda09960d6960f45235d860e2b98fc7a3a0fd9ad
Gautham Velchuru <gvelchuru@gmail.com>
2020-01-29 00:20:16 -0800

    merging

--- zsh_scripts/startup.sh
+++ zsh_scripts/startup.sh
@@ -85,7 +85,7 @@ elif [[ $IS_MAC -eq 0 ]] ; then
   alias mac_copy="nc -l 2000 | pbcopy"
 elif [[ $IS_EC2 -eq 0 ]] ; then
   brew_startup
-  alias startup="cd ~ && git_init && aptstartup && brewstartup && commonstartup"
+  alias startup="cd ~ && git_init && aptstartup && brewstartup && commonstartup && sudo swapon /dev/nvme1n1"
   export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/x86_64-linux-gnu/pkgconfig
   export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/share/pkgconfig
   export PATH=$HOME/.mozbuild/arcanist/bin:$PATH
