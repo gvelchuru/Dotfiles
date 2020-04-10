c8cd74c5805339bc0706d196c834fa8b13c0d216
Gautham Velchuru <gvelchuru@gmail.com>
2020-04-09 18:04:57 -0700

    switch to solarized

--- .hyper.js
+++ .hyper.js
@@ -9,10 +9,10 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 18,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"FiraCode Nerd Font"',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
@@ -26,30 +26,11 @@ module.exports = {
     // letter spacing as a relative unit
     letterSpacing: 0,
 
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
-
-    // terminal text color under BLOCK cursor
-    cursorAccentColor: '#000',
-
     // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: 'BLOCK',
 
     // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: false,
-
-    // color of the text
-    foregroundColor: '#fff',
-
-    // terminal background color
-    // opacity is only supported on macOS
-    backgroundColor: '#000',
-
-    // terminal selection color
-    selectionColor: 'rgba(248,28,229,0.3)',
-
-    // border color (window, tabs)
-    borderColor: '#333',
 
     // custom CSS to embed in the main window
     css: '',
@@ -67,30 +48,7 @@ module.exports = {
     showWindowControls: '',
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
-    colors: {
-      black: '#000000',
-      red: '#C51E14',
-      green: '#1DC121',
-      yellow: '#C7C329',
-      blue: '#0A2FC4',
-      magenta: '#C839C5',
-      cyan: '#20C5C6',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
-    },
-
+    padding: '',
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
     //
@@ -113,7 +71,7 @@ module.exports = {
     env: {},
 
     // set to `false` for no bell
-    bell: 'SOUND',
+    bell: 'false',
 
     // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
     copyOnSelect: false,
@@ -146,15 +104,16 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ['hyper-solarized-light'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
   localPlugins: [],
 
+
   keymaps: {
     // Example
     // 'window:devtools': 'cmd+alt+o',
   },
-};
+}
